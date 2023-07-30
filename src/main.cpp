692cf8a70ccdb820791d8cfae0b9a7ed77fed075
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-07-30 17:59:15 +0300

    change

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@
 #include <avr/power.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <SoftwareSerial.h>
 
-#define PERIOD_TIME 259200000 // 60 * 60 * 24 * 3 * 1000   // period in seconds - 3 days
-#define WORK_TIME 25000 
+#define MILLIS_CORRECT_IS_SUPPURT true
+#define __AVR_ATtiny85__
+#include <GyverButton.h>
+#include <GyverPower.h>
+
+#define PERIOD_TIME 7000 // 60 * 60 * 24 * 3 * 1000   // period in seconds - 3 days
+#define WORK_TIME 5000 
 #define PIN_MOSFET PB1
 #define PIN_BTN_MANUAL PB2
+
+GButton btnManual(PIN_BTN_MANUAL);
 
 boolean state = false;
 
@@ -18,32 +26,63 @@ volatile boolean manualFlag = false;
 #define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
 #define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
 
+SoftwareSerial serial(3, 4);
+
+void isr() {
+  // в отличие от sleepDelay, ничего вызывать не нужно!
+    // uint8_t changedbits;
+
+    // changedbits = PINB ^ portbhistory;
+    // portbhistory = PINB;
+
+    // if(changedbits & (1 << PB2))
+    // {
+    //   manualFlag = true;
+    // }
+    serial.println("ISR");
+    manualFlag = true;
+    power.wakeUp();
+}
+
 void setup() {
   // all pins - input for power saving
   for (byte i = 0; i < 6; i++) {
-    pinMode(i, INPUT);
+   // pinMode(i, INPUT);
   }
   pinMode(PIN_MOSFET, OUTPUT);
   pinMode(PIN_BTN_MANUAL, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(2), isr, FALLING);
+
+  // глубокий сон
+  power.setSleepMode(IDLE_SLEEP);
   adc_disable(); // power saving
 
-  wdt_reset(); // watchdog init
-  // 15MS, 30MS, 60MS, 120MS, 250MS, 500MS, 1S, 2S, 4S, 8S
-  wdt_enable(WDTO_4S); // watchdog conf
+  btnManual.setTickMode(AUTO);
 
-  WDTCR |= _BV(WDIE);     // enable watchdog interrupt
-  GIMSK |= (1<<INT0); // enabling the INT0 (external interrupt) 
-  PCMSK |= _BV(PCINT2); // enabling external interrupt on Pin PB2
-  sei(); // allow interruptions  
-  set_sleep_mode(SLEEP_MODE_IDLE);
+  // wdt_reset(); // watchdog init
+  // // 15MS, 30MS, 60MS, 120MS, 250MS, 500MS, 1S, 2S, 4S, 8S
+  // wdt_enable(WDTO_4S); // watchdog conf
+
+  // WDTCR |= _BV(WDIE);     // enable watchdog interrupt
+  // GIMSK |= (1<<INT0); // enabling the INT0 (external interrupt) 
+  // PCMSK |= _BV(PCINT2); // enabling external interrupt on Pin PB2
+  // sei(); // allow interruptions  
+  // set_sleep_mode(SLEEP_MODE_IDLE);
+
+  serial.begin(9600);
 }
  
 void loop() {
   static uint32_t periodTimer = 0;
   static uint32_t workTimer = 0;
+  static uint32_t tmr;
   unsigned long currentMillis = millis();
 
-  if (manualFlag) {
+  //serial.println("Loop.. " + String(millis()));
+
+  if (btnManual.isClick() || btnManual.isHold()) {
+    serial.println("Button click...");
       workTimer = currentMillis;
       periodTimer = currentMillis;
 
@@ -55,6 +94,7 @@ void loop() {
 
   if (!state) {
     if (currentMillis - periodTimer >= PERIOD_TIME) {
+      serial.println("Period...");
       periodTimer = currentMillis;
       workTimer = currentMillis;
       state = true;
@@ -63,6 +103,7 @@ void loop() {
     }
   } else {
     if (currentMillis - workTimer >= WORK_TIME) {
+      serial.println("Work...");
       workTimer = currentMillis;
       periodTimer = currentMillis;
       state = false;
@@ -71,23 +112,26 @@ void loop() {
     }
   }
 
-  sleep_enable(); // allow sleep
-  sleep_cpu();// sleep
+  // sleep_enable(); // allow sleep
+  // sleep_cpu();// sleep
+  power.sleep(SLEEP_4096MS);
+
+  serial.println("wake up!");
 }
 
-ISR (WDT_vect) {
-  WDTCR |= _BV(WDIE); // allow interrupt by watchdog
-}
+// ISR (WDT_vect) {
+//   WDTCR |= _BV(WDIE); // allow interrupt by watchdog
+// }
 
-ISR (INT0_vect)
-{
-    uint8_t changedbits;
+// ISR (INT0_vect)
+// {
+    // uint8_t changedbits;
 
-    changedbits = PINB ^ portbhistory;
-    portbhistory = PINB;
+    // changedbits = PINB ^ portbhistory;
+    // portbhistory = PINB;
 
-    if(changedbits & (1 << PB2))
-    {
-      manualFlag = true;
-    }
-}
+    // if(changedbits & (1 << PB2))
+    // {
+    //   manualFlag = true;
+    // }
+// }
