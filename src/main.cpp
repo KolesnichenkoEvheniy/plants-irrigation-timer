e1738efef55bdab1181d1fcf05ad91fadc04f4ea
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-07-31 21:31:16 +0300

    add serial debugger

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <avr/power.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <SoftwareSerial.h>
 
-#define PERIOD_TIME 259200000 // 60 * 60 * 24 * 3 * 1000   // period in seconds - 3 days
-#define WORK_TIME 25000 
+#define PERIOD_TIME 259200000//60 * 60 * 24 * 3 * 1000   // period in seconds - 3 days
+#define WORK_TIME 25000
+#define SERIAL_DEBUG_TIME 20000
 #define PIN_MOSFET PB1
 #define PIN_BTN_MANUAL PB2
 
@@ -18,11 +20,22 @@ volatile boolean manualFlag = false;
 #define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
 #define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
 
+SoftwareSerial serial(3, 4);
+
+void isr() {
+  serial.println("ISR");
+  if (!manualFlag) {
+    manualFlag = true;
+  }
+}
+
 void setup() {
   // all pins - input for power saving
-  for (byte i = 0; i < 6; i++) {
-    pinMode(i, INPUT);
-  }
+  pinMode(0, INPUT);
+  pinMode(0, INPUT);
+  pinMode(3, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
   pinMode(PIN_MOSFET, OUTPUT);
   pinMode(PIN_BTN_MANUAL, INPUT_PULLUP);
   adc_disable(); // power saving
@@ -31,30 +44,53 @@ void setup() {
   // 15MS, 30MS, 60MS, 120MS, 250MS, 500MS, 1S, 2S, 4S, 8S
   wdt_enable(WDTO_4S); // watchdog conf
 
+  attachInterrupt(digitalPinToInterrupt(2), isr, FALLING);
   WDTCR |= _BV(WDIE);     // enable watchdog interrupt
   GIMSK |= (1<<INT0); // enabling the INT0 (external interrupt) 
-  PCMSK |= _BV(PCINT2); // enabling external interrupt on Pin PB2
+  // PCMSK |= _BV(PCINT2); // enabling external interrupt on Pin PB2
   sei(); // allow interruptions  
   set_sleep_mode(SLEEP_MODE_IDLE);
+
+  serial.begin(9600);
 }
  
 void loop() {
-  static uint32_t periodTimer = 0;
-  static uint32_t workTimer = 0;
+  static unsigned long periodTimer = 0;
+  static unsigned long workTimer = 0;
+  static unsigned long debugTimer = 0;
+  static unsigned long beforeNextEvent = 0;
   unsigned long currentMillis = millis();
 
-  if (manualFlag) {
-      workTimer = currentMillis;
-      periodTimer = currentMillis;
+  if (currentMillis - debugTimer >= SERIAL_DEBUG_TIME) {
+    debugTimer = currentMillis;
 
-      state = true;
-      pinMode(PIN_MOSFET, OUTPUT);
-      digitalWrite(PIN_MOSFET, HIGH);
-      manualFlag = false;
+    if (!state){
+      serial.print("Before next irrigation: ");
+      beforeNextEvent = 0.001 * ((periodTimer + PERIOD_TIME) - currentMillis);
+      serial.print(String( beforeNextEvent));
+    } else {
+      serial.print("Before Stop: ");
+      beforeNextEvent = 0.001 * ((workTimer + WORK_TIME) - currentMillis);
+      serial.print(String( beforeNextEvent ));
+    }
+    serial.print("s");
+    serial.println("");
+  }
+
+  if (manualFlag && !state) {
+    serial.println("Button click...");
+    workTimer = currentMillis;
+    // periodTimer = currentMillis; do not reset period on manual mode
+
+    state = true;
+    pinMode(PIN_MOSFET, OUTPUT);
+    digitalWrite(PIN_MOSFET, HIGH);
+    manualFlag = false;
   }
 
   if (!state) {
     if (currentMillis - periodTimer >= PERIOD_TIME) {
+      serial.println("Start irrigation...");
       periodTimer = currentMillis;
       workTimer = currentMillis;
       state = true;
@@ -63,6 +99,7 @@ void loop() {
     }
   } else {
     if (currentMillis - workTimer >= WORK_TIME) {
+      serial.println("Stop irrigation...");
       workTimer = currentMillis;
       periodTimer = currentMillis;
       state = false;
@@ -79,15 +116,15 @@ ISR (WDT_vect) {
   WDTCR |= _BV(WDIE); // allow interrupt by watchdog
 }
 
-ISR (INT0_vect)
-{
-    uint8_t changedbits;
+// ISR (INT0_vect)
+// {
+//     uint8_t changedbits;
 
-    changedbits = PINB ^ portbhistory;
-    portbhistory = PINB;
+//     changedbits = PINB ^ portbhistory;
+//     portbhistory = PINB;
 
-    if(changedbits & (1 << PB2))
-    {
-      manualFlag = true;
-    }
-}
+//     if(changedbits & (1 << PB2))
+//     {
+//       manualFlag = true;
+//     }
+// }
