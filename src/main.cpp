1702a473bb9a8185e0c027c55a176ec5b7632ebe
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-08-11 22:24:22 +0300

    ~

--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,140 @@
-#include <Arduino.h>
-#include <avr/wdt.h>
-#include <avr/sleep.h>
-#include <avr/power.h>
-#include <avr/interrupt.h>
-#include <avr/io.h>
+
+/*
+SetTime.ino sketch to set the time of DS1307 RTC - created by Paul Stoffregen
+github.com/PaulStoffregen/DS1307RTC/blob/master/examples/SetTime/SetTime.ino
+ */
+
+#define __AVR_ATtiny85__
+
+#include <Arduino.h> 
+// #include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+#include <TinyWireM.h>
 #include <SoftwareSerial.h>
 
-#define PERIOD_TIME 5000//60 * 60 * 24 * 3 * 1000   // period in seconds - 3 days
-#define WORK_TIME 5000
-#define SERIAL_DEBUG_TIME 2000
-#define PIN_MOSFET PB1
-#define PIN_BTN_MANUAL PB2
+#define Wire TinyWireM
 
-boolean state = false;
+const char *monthName[12] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
 
-volatile uint8_t portbhistory = 0xFF;     // default is high because the pull-up
-volatile boolean manualFlag = false;
-
-#define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
-#define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
-
+tmElements_t tm;
 SoftwareSerial serial(3, 4);
 
-void isr() {
-  serial.println("ISR");
-  if (!manualFlag) {
-    manualFlag = true;
-  }
-}
+bool getTime(const char *str);
+bool getDate(const char *str);
+
+
 
 void setup() {
-  // all pins - input for power saving
-  pinMode(0, INPUT);
-  pinMode(0, INPUT);
-  pinMode(3, INPUT);
-  pinMode(5, INPUT);
-  pinMode(6, INPUT);
-  pinMode(PIN_MOSFET, OUTPUT);
-  pinMode(PIN_BTN_MANUAL, INPUT_PULLUP);
-  adc_disable(); // power saving
-
-  wdt_reset(); // watchdog init
-  // 15MS, 30MS, 60MS, 120MS, 250MS, 500MS, 1S, 2S, 4S, 8S
-  wdt_enable(WDTO_4S); // watchdog conf
-
-  attachInterrupt(digitalPinToInterrupt(2), isr, FALLING);
-  WDTCR |= _BV(WDIE);     // enable watchdog interrupt
-  GIMSK |= (1<<INT0); // enabling the INT0 (external interrupt) 
-  // PCMSK |= _BV(PCINT2); // enabling external interrupt on Pin PB2
-  sei(); // allow interruptions  
-  set_sleep_mode(SLEEP_MODE_IDLE);
+  bool parse=false;
+  bool config=false;
 
   serial.begin(9600);
+  while (!serial) ; // wait for Arduino Serial Monitor
+  delay(3000);
+  Wire.begin();
+
+  // serial.println("Initial");
+  // serial.println(__DATE__);
+  // serial.println(tm.Hour);
+  // serial.println(tm.Minute);
+  // setTime(8,29,0,1,1,11);
+  // // get the date and time the compiler was run
+  if (getDate(__DATE__) && getTime(__TIME__)) {
+    parse = true;
+    // and configure the RTC with this info
+    // serial.println("before");
+  
+    // Wire.beginTransmission(0x68);
+    // Wire.end();
+    // serial.println("after");
+    // if (RTC.write(tm)) {
+    //   config = true;
+    //   serial.println("YES");
+    // } else {
+    //   serial.println("still no");
+    // }
+    
+
+      delay(500);
+      setSyncProvider(RTC.get);  
+
+     // time_t time = RTC.get();
+    if (timeStatus() != timeSet) 
+      serial.println("Unable to sync with the RTC");
+    else
+      serial.println("RTC has set the system time");
+
+      serial.println("Chip status: ");
+    serial.println(RTC.chipPresent());
+    
+   
+  }
+
+
+  
+  serial.print("TEST d: ");
+  serial.println(tm.Year);
+  // if (parse && config) {
+  //   serial.print("DS1307 configured Time=");
+  //   serial.print(__TIME__);
+  //   serial.print(", Date=");
+  //   serial.println(__DATE__);
+  // } else if (parse) {
+  //   serial.println("DS1307 Communication Error :-{");
+  //   serial.println("Please check your circuitry");
+  // } else {
+  //   serial.print("Could not parse info from the compiler, Time=\"");
+  //   serial.print(__TIME__);
+  //   serial.print("\", Date=\"");
+  //   serial.print(__DATE__);
+  //   serial.println("\"");
+  // }
 }
- 
+
+void printDigits(int digits) {
+  serial.print(":");
+  if (digits < 10)
+    serial.print('0');
+  serial.print(digits);
+}
+
 void loop() {
-  static unsigned long periodTimer = 0;
-  static unsigned long workTimer = 0;
-  static unsigned long debugTimer = 0;
-  static unsigned long beforeNextEvent = 0;
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - debugTimer >= SERIAL_DEBUG_TIME) {
-    debugTimer = currentMillis;
-
-    if (!state){
-      serial.print("Before next irrigation: ");
-      beforeNextEvent = 0.001 * ((periodTimer + PERIOD_TIME) - currentMillis);
-      serial.print(String( beforeNextEvent));
-    } else {
-      serial.print("Before Stop: ");
-      beforeNextEvent = 0.001 * ((workTimer + WORK_TIME) - currentMillis);
-      serial.print(String( beforeNextEvent ));
-    }
-    serial.print("s");
-    serial.println("");
-  }
-
-  if (manualFlag && !state) {
-    serial.println("Button click...");
-    workTimer = currentMillis;
-    // periodTimer = currentMillis; do not reset period on manual mode
-
-    state = true;
-    pinMode(PIN_MOSFET, OUTPUT);
-    digitalWrite(PIN_MOSFET, HIGH);
-    manualFlag = false;
-  }
-
-  if (!state) {
-    if (currentMillis - periodTimer >= PERIOD_TIME) {
-      serial.println("Start irrigation...");
-      periodTimer = currentMillis;
-      workTimer = currentMillis;
-      state = true;
-      pinMode(PIN_MOSFET, OUTPUT);
-      digitalWrite(PIN_MOSFET, HIGH);
-    }
-  } else {
-    if (currentMillis - workTimer >= WORK_TIME) {
-      serial.println("Stop irrigation...");
-      workTimer = currentMillis;
-      periodTimer = currentMillis;
-      state = false;
-      digitalWrite(PIN_MOSFET, LOW);
-      pinMode(PIN_MOSFET, INPUT); // power saving
-    }
-  }
-
-  sleep_enable(); // allow sleep
-  sleep_cpu();// sleep
+  delay(1000);
+  serial.print("Configured Time=");
+    serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  serial.println();
 }
 
-ISR (WDT_vect) {
-  WDTCR |= _BV(WDIE); // allow interrupt by watchdog
+bool getTime(const char *str)
+{
+  int Hour, Min, Sec;
+
+  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+  tm.Hour = Hour;
+  tm.Minute = Min;
+  tm.Second = Sec;
+  return true;
 }
 
-// ISR (INT0_vect)
-// {
-//     uint8_t changedbits;
+bool getDate(const char *str)
+{
+  char Month[12];
+  int Day, Year;
+  uint8_t monthIndex;
 
-//     changedbits = PINB ^ portbhistory;
-//     portbhistory = PINB;
-
-//     if(changedbits & (1 << PB2))
-//     {
-//       manualFlag = true;
-//     }
-// }
+  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
+  for (monthIndex = 0; monthIndex < 12; monthIndex++) {
+    if (strcmp(Month, monthName[monthIndex]) == 0) break;
+  }
+  if (monthIndex >= 12) return false;
+  tm.Day = Day;
+  tm.Month = monthIndex + 1;
+  tm.Year = CalendarYrToTm(Year);
+  return true;
+}
