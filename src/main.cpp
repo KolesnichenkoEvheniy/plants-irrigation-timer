f05483e0352d35cc7f9b754b567f8f84f97ab1b2
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-08-12 11:15:46 +0300

    setup pin3 as external int & use RST as RX pin

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,15 @@ github.com/PaulStoffregen/DS1307RTC/blob/master/examples/SetTime/SetTime.ino
 #include <SoftwareSerial.h>
 
 #define PIN_MOSFET PB1
-#define PIN_BTN_MANUAL 3
+#define PIN_BTN_MANUAL PB3
 #define PERIOD_TIME 7000//60 * 60 * 24 * 3 * 1000   // period in seconds - 3 days
 #define WORK_TIME 3000
 #define SERIAL_DEBUG_TIME 2000
 
 #define Wire TinyWireM
+
+#define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
+#define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
 
 const char *monthName[12] = {
   "Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -29,7 +32,7 @@ const char *monthName[12] = {
 };
 
 tmElements_t tm;
-SoftwareSerial serial(3, 4);
+SoftwareSerial serial(1, 4);
 // SoftwareSerial serial(0, 0);
 volatile uint8_t portbhistory = 0xFF;     // default is high because the pull-up
 volatile boolean manualFlag = false;
@@ -38,10 +41,10 @@ boolean state = false;
 bool getTime(const char *str);
 bool getDate(const char *str);
 
-void isr() {
-  serial.println("ISR");
-  manualFlag = true;
-}
+// void isr() {
+//   serial.println("ISR");
+//   manualFlag = true;
+// }
 
 void setup() {
   bool parse=false;
@@ -49,38 +52,16 @@ void setup() {
 
   serial.begin(9600);
   while (!serial) ; // wait for Arduino Serial Monitor
-  delay(3000);
+  delay(1000);
   Wire.begin();
 
   pinMode(PIN_MOSFET, OUTPUT);
   pinMode(PIN_BTN_MANUAL, INPUT_PULLUP);
+  adc_disable();
 
-  // serial.println("Initial");
-  // serial.println(__DATE__);
-  // serial.println(tm.Hour);
-  // serial.println(tm.Minute);
-  // setTime(8,29,0,1,1,11);
-  // // get the date and time the compiler was run
-  // if (getDate(__DATE__) && getTime(__TIME__)) {}
-    // parse = true;
-    // and configure the RTC with this info
-    // serial.println("before");
-  
-    // Wire.beginTransmission(0x68);
-    // Wire.end();
-    // serial.println("after");
-    // if (RTC.write(tm)) {
-    //   config = true;
-    //   serial.println("YES");
-    // } else {
-    //   serial.println("still no");
-    // }
-    
+  delay(500);
+  setSyncProvider(RTC.get);  
 
-    delay(500);
-    setSyncProvider(RTC.get);  
-
-    // time_t time = RTC.get();
   if (timeStatus() != timeSet) {
     serial.println("Unable to sync with the RTC");
   } else {
@@ -89,30 +70,12 @@ void setup() {
 
   serial.println("Chip status: ");
   serial.println(RTC.chipPresent());
-  
-  serial.print("TEST d: ");
-  serial.println(tm.Year);
-  // if (parse && config) {
-  //   serial.print("DS1307 configured Time=");
-  //   serial.print(__TIME__);
-  //   serial.print(", Date=");
-  //   serial.println(__DATE__);
-  // } else if (parse) {
-  //   serial.println("DS1307 Communication Error :-{");
-  //   serial.println("Please check your circuitry");
-  // } else {
-  //   serial.print("Could not parse info from the compiler, Time=\"");
-  //   serial.print(__TIME__);
-  //   serial.print("\", Date=\"");
-  //   serial.print(__DATE__);
-  //   serial.println("\"");
-  // }
 
   wdt_reset(); // watchdog init
   // 15MS, 30MS, 60MS, 120MS, 250MS, 500MS, 1S, 2S, 4S, 8S
   wdt_enable(WDTO_4S); // watchdog conf
 
-  attachInterrupt(digitalPinToInterrupt(PIN_BTN_MANUAL), isr, FALLING);
+  // attachInterrupt(digitalPinToInterrupt(PIN_BTN_MANUAL), isr, FALLING);
   WDTCR |= _BV(WDIE);     // enable watchdog interrupt
   GIMSK |= (1<<INT0); // enabling the INT0 (external interrupt) 
   PCMSK |= _BV(PCINT3); // enabling external interrupt on Pin PB2
@@ -128,13 +91,6 @@ void printDigits(int digits) {
 }
 
 void loop() {
-  // delay(1000);
-  // serial.print("Configured Time=");
-  //   serial.print(hour());
-  // printDigits(minute());
-  // printDigits(second());
-  // serial.println();
-
   static unsigned long periodTimer = 0;
   static unsigned long workTimer = 0;
   static unsigned long debugTimer = 0;
@@ -165,10 +121,9 @@ void loop() {
     // manualFlag = true;
   }
 
-
-serial.println(digitalRead(PIN_BTN_MANUAL) == LOW ? "MNL" : "NO");
-  if (manualFlag || digitalRead(PIN_BTN_MANUAL) == LOW/*&& !state*/) {
-    serial.println("Button click...");
+  //serial.println(digitalRead(PIN_BTN_MANUAL) == LOW ? "MNL" : "NO");
+  if (manualFlag/*&& !state*/) {
+    serial.println("!!!Button click...");
     //serial.println(digitalRead(PIN_BTN_MANUAL) ? "MNL" : "NO");
     // workTimer = currentMillis;
     // periodTimer = currentMillis; do not reset period on manual mode
@@ -233,4 +188,18 @@ bool getDate(const char *str)
 
 ISR (WDT_vect) {
   WDTCR |= _BV(WDIE); // allow interrupt by watchdog
+}
+
+ISR (INT0_vect)
+{
+    uint8_t changedbits;
+
+    changedbits = PINB ^ portbhistory;
+    portbhistory = PINB;
+
+    if(changedbits & (1 << PB3))
+    {
+      serial.println("ISR INT0_vecxt");
+      manualFlag = true;
+    }
 }
