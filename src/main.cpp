91c95c9149b21bf36bb30673736a0ba2d9188e9d
Yevhenii Kolesnichenko <kolesnichenko.yevhenii@pdffiller.team>
2023-08-12 12:32:24 +0300

    small impr

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <avr/wdt.h>
 #include <avr/sleep.h>
 #include <avr/power.h>
-#include <TimeLib.h>
+#include <TimeAlarms.h>
 #include <DS1307RTC.h>
 #include <TinyWireM.h>
 #include <SoftwareSerial.h>
@@ -34,9 +34,6 @@ bool getTime(const char *str);
 bool getDate(const char *str);
 
 void setup() {
-  bool parse=false;
-  bool config=false;
-
   serial.begin(9600);
   while (!serial) ; // wait for Arduino Serial Monitor
   delay(1000);
@@ -145,33 +142,33 @@ void loop() {
   sleep_cpu();// sleep
 }
 
-bool getTime(const char *str)
-{
-  int Hour, Min, Sec;
+// bool getTime(const char *str)
+// {
+//   int Hour, Min, Sec;
 
-  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
-  tm.Hour = Hour;
-  tm.Minute = Min;
-  tm.Second = Sec;
-  return true;
-}
+//   if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+//   tm.Hour = Hour;
+//   tm.Minute = Min;
+//   tm.Second = Sec;
+//   return true;
+// }
 
-bool getDate(const char *str)
-{
-  char Month[12];
-  int Day, Year;
-  uint8_t monthIndex;
+// bool getDate(const char *str)
+// {
+//   char Month[12];
+//   int Day, Year;
+//   uint8_t monthIndex;
 
-  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
-  for (monthIndex = 0; monthIndex < 12; monthIndex++) {
-    if (strcmp(Month, monthName[monthIndex]) == 0) break;
-  }
-  if (monthIndex >= 12) return false;
-  tm.Day = Day;
-  tm.Month = monthIndex + 1;
-  tm.Year = CalendarYrToTm(Year);
-  return true;
-}
+//   if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
+//   for (monthIndex = 0; monthIndex < 12; monthIndex++) {
+//     if (strcmp(Month, monthName[monthIndex]) == 0) break;
+//   }
+//   if (monthIndex >= 12) return false;
+//   tm.Day = Day;
+//   tm.Month = monthIndex + 1;
+//   tm.Year = CalendarYrToTm(Year);
+//   return true;
+// }
 
 ISR (WDT_vect) {
   WDTCR |= _BV(WDIE); // allow interrupt by watchdog
